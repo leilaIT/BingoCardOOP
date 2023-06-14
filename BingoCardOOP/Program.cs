3b92c95bbcb041b2959c1f5b115b1a5a98e62af0
leilaIT <delarealeila@gmail.com>
2023-06-14 10:16:26 +0800

    improved duplicate checker

--- BingoCardOOP/Program.cs
+++ BingoCardOOP/Program.cs
@@ -9,41 +9,6 @@ namespace BingoCardOOP
 {
     internal class Program
     {
-        //static void Main(string[] args)
-        //{
-        //    int[] cardNum = new int[] { };
-        //    int[] bCard = new int[] { };
-
-        //    for (int i = 0; i < cardNum; i++) //tells the program how many cards to generate
-        //    {
-        //        //initialize card contents to 0
-        //        for (int x = 0; x < bCard.GetLength(0); x++) //card row
-        //        {
-        //            for (int y = 0; y < bCard.GetLength(1); y++) //card col
-        //            {
-        //                bCard[x, y] = 0;
-        //            }
-        //        }
-
-        //        //bingo card generation
-        //        for (int x = 0; x < bCard.GetLength(0); x++) //card row
-        //        {
-        //            listNum.Clear();
-        //            for (int c = 1; c < 16; c++) //for generating possible numbers from 1-15
-        //            {
-        //                listNum.Add(c);
-        //            }
-
-        //            for (int y = 0; y < bCard.GetLength(1); y++) //card col
-        //            {
-        //                //generate numbers
-        //                num = rnd.Next(0, listNum.Count);
-        //                bCard[y, x] = listNum[num];
-        //                listNum.RemoveAt(num);
-        //            }
-        //        }
-        //    }
-        //}
         static void Main(string[] args)
         {
             int[][,] cards = { };
@@ -51,10 +16,10 @@ namespace BingoCardOOP
             bool cont = true;
             Random rnd = new Random();
             cards = new int[getNumFromUser("Please input the number of cards you wish to be generated : ")][,];
-            
+
             for (int x = 0; x < cards.Length; x++)
                 cards[x] = generateCard(rnd);
-            
+
             while (cont)
             {
                 Console.Clear();
@@ -98,18 +63,21 @@ namespace BingoCardOOP
         static int[,] generateCard(Random rnd)
         {
             int[,] cards = new int[5, 5];
+            List<int> listNum = new List<int>();
             int num = 0;
 
             for (int x = 0; x < cards.GetLength(0); x++)
             {
+                listNum.Clear();
+                for (int c = 1; c < 16; c++)
+                {
+                    listNum.Add(c);
+                }
                 for (int y = 0; y < cards.GetLength(1); y++)
                 {
-                    num = rnd.Next(0, 15) + 1;
-                    if (num > 15)
-                    {
-                        Console.ReadKey();
-                    }
-                    cards[x, y] = num;
+                    num = rnd.Next(0, listNum.Count);
+                    cards[y, x] = listNum[num];
+                    listNum.RemoveAt(num);
                 }
             }
             return cards;
@@ -119,9 +87,8 @@ namespace BingoCardOOP
             Console.WriteLine("B\tI\tN\tG\tO");
             for (int x = 0; x < card.GetLength(0); x++)
             {
-                for (int y = 0; y < card.GetLength(1);  y++)
+                for (int y = 0; y < card.GetLength(1); y++)
                 {
-                    //card[x, y] = card[x, y] + (y * 15);
                     Console.Write((card[x, y] + (y * 15)) + "\t");
                 }
                 Console.WriteLine();
